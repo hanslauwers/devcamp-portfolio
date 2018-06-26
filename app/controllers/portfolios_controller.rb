b2e28c58b8cced20576eccc848d0836ca1266791
Hans Lauwers <hans.lauwers@hotmail.com>
2018-06-26 11:46:01 -0700

    Added custom layouts for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,67 +1,69 @@
 class PortfoliosController < ApplicationController
-def index
-  @portfolio_items = Portfolio.all
-end
+  layout "portfolio"
 
-def angular
-  @angular_portfolio_items = Portfolio.angular
-end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-def new
-  @portfolio_item = Portfolio.new
-  3.times {
-    @portfolio_item.technologies.build
-  }
-end
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
 
-def create
-  @portfolio_item = Portfolio.new(portfolio_params)
+  def new
+    @portfolio_item = Portfolio.new
+    3.times {
+      @portfolio_item.technologies.build
+    }
+  end
 
-  respond_to do |format|
-    if @portfolio_item.save
-      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-    else
-      format.html { render :new }
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
     end
   end
-end
 
-def edit
-  @portfolio_item = Portfolio.find(params[:id])
-end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-def update
-  @portfolio_item = Portfolio.find(params[:id])
-  respond_to do |format|
-    if @portfolio_item.update(portfolio_params)
-      format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
-    else
-      format.html { render :edit }
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
   end
-end
 
-def show
-  @portfolio_item = Portfolio.find(params[:id])
-end
-
-def destroy
-  @portfolio_item = Portfolio.find(params[:id])
-  @portfolio_item.destroy
-
-  respond_to do |format|
-    format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully removed.' }
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
-end
 
-private
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
 
-def portfolio_params
-  params.require(:portfolio).permit(:title, 
-                                    :subtitle,
-                                    :body,
-                                    technologies_attributes: [:name]
-                                    )
-end
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully removed.' }
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                      )
+  end
 
 end
