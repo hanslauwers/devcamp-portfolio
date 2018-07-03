343e28032233c87085429cc1bf4fe4c3fb4cef23
Hans Lauwers <hans.lauwers@hotmail.com>
2018-07-02 19:57:18 -0700

    Implemented navigation refactor and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,37 @@ module ApplicationHelper
   def set_copyright
     LauwersCopyrightTool::Renderer.copyright 'Hans Lauwers', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },{
+        url: about_me_path,
+        title: "About me"
+      },{
+        url: contact_path,
+        title: "Contact"
+      },{
+        url: portfolios_path,
+        title: "Portfolios"
+      },{
+        url: blogs_path,
+        title: "Blogs"
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
