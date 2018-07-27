8b1d7e696d68eb5eacb8e183ca0f77263aa93188
Hans Lauwers <hans.lauwers@hotmail.com>
2018-07-27 14:50:00 -0700

    Added scope for topics that are linked to at least one blog

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   layout "blog"
+  before_action :set_sidebar_topics
 
   def show
     @topic = Topic.find(params[:id])
@@ -9,6 +10,12 @@ class TopicsController < ApplicationController
     else
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
+  end
+
+  private
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
   end
 
 end
